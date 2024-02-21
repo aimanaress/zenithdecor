ef7c8aedc1683ea5c242ee167ad382b54a3a48b5
Aiman Ares <aaimanaress@gmail.com>
2024-02-21 15:00:03 +0800

    Create footer component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ import React from "react";
 import NavBar from "./components/Navigation/NavBar";
 import Hero from "./components/Hero/Hero";
 import Cards from "./components/Card/Cards";
+import Collection from "./components/Collection/Collection";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <React.Fragment>
+    <div className="bg-background">
       <NavBar />
       <div className="flex flex-col">
-        <div className="flex bg-background items-center justify-center">
+        <div className="flex items-center justify-center">
           <div className="w-[70rem]">
             <Hero />
             <Cards />
+            <Collection />
           </div>
         </div>
       </div>
-    </React.Fragment>
+      <Footer />
+    </div>
   );
 }
 
